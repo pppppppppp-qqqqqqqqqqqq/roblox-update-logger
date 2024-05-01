63ac522b0a30766ad4e3795569d7ad4893ff8d10
pppppppppp-qqqqqqqqqqqq <126934644+pppppppppp-qqqqqqqqqqqq@users.noreply.github.com>
2024-05-01 15:38:16 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import requests, json, time
+
+def main()
+    session = requests.Session()
+
+    def checkversion()
+        url = "https://clientsettings.roblox.com/v2/client-version/WindowsPlayer/channel/LIVE"
+        response = session.get(url)
+        decoded = json.loads(response.text)
+
+        return decoded["clientVersionUpload"]
+
+    currentversion =  checkversion()
+
+    while True:
+        if currentversion !=  checkversion() then
+            print("roblox has updated nigga" + f"https://rdd.latte.to/?channel=LIVE&binaryType=WindowsPlayer&version={currentversion}")
+            currentversion = checkversion()
+
+    time.sleep(60)
+
+if __name__ == "__main__":
+    main()
